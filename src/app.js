bf4577a584188f974ea5ee1f1105e402705cd8dd
Alex <alexburnitdown@gmail.com.com>
2025-06-05 13:13:19 +0300

    3 commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Hello CI/CD World111!', timestamp: new Date().toISOString() });
+  res.json({ message: 'Hello CI/CD World!', timestamp: new Date().toISOString() });
 });
 
 app.get('/health', (req, res) => {
